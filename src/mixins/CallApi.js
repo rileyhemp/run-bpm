4a8f1cc22bff84e61d0923c2ee0fd485ef0c167c
Riley Hemphill <rileyhemp@gmail.com>
2020-01-17 17:20:21 -0500

    Added global api call mixin

---
+++ src/mixins/CallApi.js
@@ -0,0 +1,17 @@
+export default {
+	methods: {
+		call(method, query, options) {
+			return this.$http
+				.get("http://localhost:3000/api", {
+					params: {
+						method: method,
+						query: query,
+						options: options
+					}
+				})
+				.then(function (data) {
+					return data.data;
+				});
+		}
+	}
+}
