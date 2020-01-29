54142e80f29deb2ecfd476f29687dbc6b3db458d
Riley Hemphill <rileyhemp@gmail.com>
2020-01-29 17:40:37 -0500

    Added dots by avatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,16 +2,16 @@
 	<v-app-bar flat>
 		<v-toolbar-title>{{pageTitle}}</v-toolbar-title>
 		<v-spacer />
-		<user-profile v-bind="$attrs" />
+		<user-avatar v-bind="$attrs" />
 	</v-app-bar>
 </template>
 
 <script>
-import UserProfile from "../components/UserProfile";
+import UserAvatar from "../components/UserAvatar";
 export default {
 	name: "Header",
 	components: {
-		"user-profile": UserProfile
+		"user-avatar": UserAvatar
 	},
 	props: ["pageTitle"]
 };
