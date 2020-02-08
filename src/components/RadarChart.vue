f18e387f1262c744aa209939e61fbf9df3d6d9d9
Riley Hemphill <rileyhemp@gmail.com>
2020-02-07 19:40:17 -0500

    Added theme colors to graph

--- src/components/RadarChart.vue
+++ src/components/RadarChart.vue
@@ -3,16 +3,16 @@
 </template>
 
 <script>
-// import RadarChart from "../scripts/radarChart";
 //eslint-disable-next-line
-import { scale } from "d3/d3.min.js";
-import RadarChart from "../scripts/radarChart";
 const margin = { top: 50, right: 50, bottom: 50, left: 50 },
 	width = Math.min(700, window.innerWidth - 50) - margin.left - margin.right,
 	height = Math.min(
 		width,
 		window.innerHeight - margin.top - margin.bottom - 20
 	);
+import { scale } from "d3/d3.min.js";
+import RadarChart from "../scripts/radarChart";
+import colors from "vuetify/lib/util/colors";
 export default {
 	name: "radar-chart",
 	props: ["chartData", "chartReady"],
@@ -24,7 +24,13 @@ export default {
 				margin: margin,
 				maxValue: 0.2,
 				roundStrokes: true,
-				color: scale.ordinal().range(["#EDC951", "#CC333F", "#00A0B0"])
+				color: scale
+					.ordinal()
+					.range([
+						colors.blue.lighten2,
+						colors.red.lighten2,
+						colors.green.lighten2
+					])
 			}
 		};
 	},
--- src/scripts/radarChart.js
+++ src/scripts/radarChart.js
@@ -131,7 +131,7 @@ function RadarChart(id, data, options) {
 	//Append the labels at each axis
 	axis.append("text")
 		.attr("class", "legend")
-		.style("font-size", "12px")
+		.style("font-size", "14px")
 		.attr("text-anchor", "middle")
 		.attr("dy", "0.35em")
 		.attr("x", function (d, i) { return rScale(maxValue * cfg.labelFactor) * Math.cos(angleSlice * i - Math.PI / 2); })
