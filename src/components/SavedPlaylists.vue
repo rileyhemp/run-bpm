52a255d5eb1771fd73958d3d04ef20fadcbf1da6
Riley Hemphill <rileyhemp@gmail.com>
2020-02-20 05:26:29 -0500

    Styling home page 10% done

--- src/components/SavedPlaylists.vue
+++ src/components/SavedPlaylists.vue
@@ -1,20 +1,52 @@
 <template>
-	<v-list two-line v-if="$attrs.savedPlaylists.length">
-		<v-list-item v-for="playlist in $attrs.savedPlaylists" :key="playlist.key">
-			<v-list-item-content>
-				<v-list-item-title>{{playlist.metadata.name}}</v-list-item-title>
-				<v-list-item-subtitle>{{playlist.metadata.tracks}} Tracks, {{playlist.metadata.lowBPM}}–{{playlist.metadata.highBPM}}bpm</v-list-item-subtitle>
-			</v-list-item-content>
-			<v-spacer />
-			<v-btn icon>
-				<v-icon>mdi-pencil</v-icon>
-			</v-btn>
-		</v-list-item>
-	</v-list>
+	<div>
+		<v-list two-line v-if="!$attrs.savedPlaylists.length">
+			<v-list-item>
+				<v-list-item-content>
+					<v-list-item-title>Nothing here yet...</v-list-item-title>
+					<v-list-item-subtitle>Tap the plus to get started</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
+
+		<v-list two-line v-if="$attrs.savedPlaylists.length">
+			<v-list-item v-for="playlist in $attrs.savedPlaylists" :key="playlist.key">
+				<v-list-item-content>
+					<v-list-item-title>{{playlist.metadata.name}}</v-list-item-title>
+					<v-list-item-subtitle>{{playlist.metadata.tracks}} Tracks, {{playlist.metadata.lowBPM}}–{{playlist.metadata.highBPM}}bpm</v-list-item-subtitle>
+				</v-list-item-content>
+				<v-spacer />
+				<v-btn icon>
+					<v-icon>mdi-pencil</v-icon>
+				</v-btn>
+				<v-dialog v-model="dialog" persistent max-width="290">
+					<template v-slot:activator="{ on }">
+						<v-btn icon v-on="on">
+							<v-icon>mdi-delete</v-icon>
+						</v-btn>
+					</template>
+					<v-card>
+						<v-card-title class="headline">Are you sure?</v-card-title>
+						<v-card-text>This action cannot be undone</v-card-text>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn color="green darken-1" text @click="dialog = false">Back</v-btn>
+							<v-btn color="green darken-1" text @click="dialog = false">Delete</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
+			</v-list-item>
+		</v-list>
+	</div>
 </template>
 
 <script>
 export default {
-	components: {}
+	components: {},
+	data() {
+		return {
+			dialog: false
+		};
+	}
 };
 </script>
