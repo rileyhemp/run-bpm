ae235aa4130aa329d4ffbd6ce2619d87ad191793
Riley Hemphill <rileyhemp@gmail.com>
2020-02-22 13:18:34 -0500

    Started player container

---
+++ src/components/containers/Player.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="player px-4">
+		<v-btn-toggle>
+			<v-btn class="player-control" icon text>
+				<v-icon>mdi-play</v-icon>
+			</v-btn>
+		</v-btn-toggle>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.player {
+	position: fixed;
+	bottom: 0;
+	height: 80px;
+	border-top: 1px solid black;
+	display: flex;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
+}
+
+.player-control {
+	border-radius: 50%;
+}
+</style>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -3,6 +3,7 @@
 		<add-button />
 		<v-subheader>My Playlists</v-subheader>
 		<saved-playlists v-bind="$attrs" @updateUserInfo="updateUserInfo" @play="play($event)" />
+		<v-spacer />
 		<player />
 	</div>
 </template>
@@ -10,11 +11,13 @@
 <script>
 import AddNewButtonVue from "../specialized/AddNewButton.vue";
 import SavedPlaylists from "../containers/SavedPlaylists";
+import Player from "../containers/Player";
 
 export default {
 	components: {
 		"add-button": AddNewButtonVue,
-		"saved-playlists": SavedPlaylists
+		"saved-playlists": SavedPlaylists,
+		player: Player
 	},
 	data: function() {
 		return {
@@ -35,5 +38,8 @@ export default {
 <style scoped>
 .v-subheader {
 	margin: 8px 0 -24px 0;
+}
+.dashboard-wrapper {
+	height: 100%;
 }
 </style>
