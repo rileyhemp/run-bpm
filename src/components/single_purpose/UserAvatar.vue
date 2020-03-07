efb299de59a0e1d658cd9146d09871f13813fa9a
Riley Hemphill <rileyhemp@gmail.com>
2020-03-06 23:31:06 -0500

    Added menu

--- src/components/single_purpose/UserAvatar.vue
+++ src/components/single_purpose/UserAvatar.vue
@@ -1,16 +1,33 @@
 <template>
-	<v-btn icon class="pr-2">
-		<v-avatar v-if="this.$attrs.user" color="primary" size="42px">
-			<span v-if="!this.hasAvatar" style="color:white">{{getInitials(this.$attrs.user.display_name)}}</span>
-			<img v-if="this.hasAvatar" :src="this.hasAvatar ? this.$attrs.user.images[0].url : null" />
-		</v-avatar>
-	</v-btn>
+	<div class="user-avatar">
+		<v-menu v-model="showMenu" :nudge-bottom="56">
+			<v-list class="text-right">
+				<v-list-item>
+					<v-list-item-title>Connected as {{ this.$attrs.user.display_name }}</v-list-item-title>
+				</v-list-item>
+				<v-list-item @click="openSpotifyProfile">
+					<v-list-item-title>View on Spotify</v-list-item-title>
+				</v-list-item>
+				<v-list-item>
+					<v-list-item-title>Logout</v-list-item-title>
+				</v-list-item>
+			</v-list>
+		</v-menu>
+		<v-btn icon @click="showMenu = !showMenu">
+			<v-avatar v-if="this.$attrs.user" color="primary" size="42px">
+				<span v-if="!this.hasAvatar" style="color:white">{{ getInitials(this.$attrs.user.display_name) }}</span>
+				<img v-if="this.hasAvatar" :src="this.hasAvatar ? this.$attrs.user.images[0].url : null" />
+			</v-avatar>
+		</v-btn>
+	</div>
 </template>
 
 <script>
 export default {
 	data: function() {
-		return {};
+		return {
+			showMenu: false
+		};
 	},
 	methods: {
 		getInitials(displayName) {
@@ -18,12 +35,13 @@ export default {
 				const names = displayName.split(" ");
 				let initials = names[0].substring(0, 1).toUpperCase();
 				if (names.length > 1) {
-					initials += names[names.length - 1]
-						.substring(0, 1)
-						.toUpperCase();
+					initials += names[names.length - 1].substring(0, 1).toUpperCase();
 				}
 				return initials;
 			}
+		},
+		openSpotifyProfile() {
+			window.open(this.$attrs.user.external_urls.spotify);
 		}
 	},
 	computed: {
@@ -39,3 +57,10 @@ export default {
 	}
 };
 </script>
+
+<style>
+.v-menu__content {
+	left: unset !important;
+	right: 0px;
+}
+</style>
