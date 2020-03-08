74a1232ac331019925a6214659d0903c8fc51f27
Riley Hemphill <rileyhemp@gmail.com>
2020-03-08 03:44:58 -0400

    Added dark theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	theme: {
+		dark: true,
+		themes: {
+			dark: {
+				primary: "#1DB954",
+				secondary: "#03DAC6",
+				accent: "#3700B3",
+				error: "#CF6679"
+			}
+		}
+	}
 });
