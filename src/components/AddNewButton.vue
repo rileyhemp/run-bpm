1f91e405417caab834da1a1d360cf97953352373
Riley Hemphill <rileyhemp@gmail.com>
2020-03-29 18:16:30 -0400

    Version 1.1

--- src/components/AddNewButton.vue
+++ src/components/AddNewButton.vue
@@ -1,6 +1,6 @@
 <template>
-	<v-row class="mx-4 py-1">
-		<v-spacer />
+	<v-row class="mx-4 py-1" :class="$vuetify.breakpoint.mdAndUp ? 'mt-4' : null">
+		<v-spacer v-if="!$vuetify.breakpoint.mdAndUp" />
 		<v-btn fab color="primary" medium to="/import">
 			<v-icon>mdi-plus</v-icon>
 		</v-btn>
