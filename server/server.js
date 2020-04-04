033e791c7e244c29b7820dfc5874609c81b420ef
Riley Hemphill <rileyhemp@gmail.com>
2020-04-04 14:57:41 -0400

    updating server.js for development

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ const DatabasePath = __dirname + "/db/new.db";
 const credentials = {
 	clientId: "dd71362980ad40bb9820af4e02f5c39e",
 	clientSecret: "515a0f00287745c19c006ce63af4d7b6",
-	redirectUri: "https://runbpm.app/connect"
+	// redirectUri: "https://runbpm.app/connect"
+	redirectUri: "localhost:8080/connect"
 };
 
 const scopes = [
@@ -27,7 +28,8 @@ const scopes = [
 const spotifyApi = new SpotifyWebApi(credentials);
 
 const allowCrossDomain = function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "https://runbpm.app");
+	// res.header("Access-Control-Allow-Origin", "https://runbpm.app");
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "*");
 	res.header("Access-Control-Allow-Headers", "*");
 	next();
