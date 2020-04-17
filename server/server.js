1a3135c42739c8cc00281a5a95b44ae584948caa
Riley Hemphill <rileyhemp@gmail.com>
2020-04-17 02:21:48 -0400

    added footer

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const credentials = {
 	clientId: "dd71362980ad40bb9820af4e02f5c39e",
 	clientSecret: "515a0f00287745c19c006ce63af4d7b6",
 	// redirectUri: "https://runbpm.app/connect"
-	redirectUri: "http://localhost:8080/connect"
+	redirectUri: "http://runbpm.app/connect",
 };
 
 const scopes = [
@@ -22,7 +22,7 @@ const scopes = [
 	"playlist-modify-private",
 	"user-read-playback-state",
 	"user-read-currently-playing",
-	"user-modify-playback-state"
+	"user-modify-playback-state",
 ];
 
 const spotifyApi = new SpotifyWebApi(credentials);
@@ -70,10 +70,10 @@ function accessToken(user) {
 			api.setAccessToken(user.accessToken);
 			api.setRefreshToken(user.refreshToken);
 			api.refreshAccessToken()
-				.then(data => {
+				.then((data) => {
 					resolve(data.body["access_token"]);
 				})
-				.catch(err => reject("Could not refresh access token", err));
+				.catch((err) => reject("Could not refresh access token", err));
 		}
 	});
 }
@@ -90,15 +90,15 @@ app.get("/get-user-data", async function(req, res) {
 	const api = spotifyApiWithToken(token);
 	//Get basic account information
 	api.getMe()
-		.then(data => {
+		.then((data) => {
 			let userData = data.body;
 			//Use account information to get playlists
 			api.getUserPlaylists(userData.id)
-				.then(data => {
+				.then((data) => {
 					let userPlaylists = data.body;
 					//Get a list of active devices
 					api.getMyDevices()
-						.then(data => {
+						.then((data) => {
 							let userDevices = data.body;
 							//Return data to front end, including current access token
 							res.send({
@@ -108,15 +108,15 @@ app.get("/get-user-data", async function(req, res) {
 								userCredentials: {
 									token: token,
 									//Access tokens technically expire after one hour, but we'll request a new one early just to be safe.
-									expiresAt: new Date().getTime() + 3000000
-								}
+									expiresAt: new Date().getTime() + 3000000,
+								},
 							});
 						})
-						.catch(err => res.status(err.statusCode).send(err));
+						.catch((err) => res.status(err.statusCode).send(err));
 				})
-				.catch(err => res.status(err.statusCode).send(err));
+				.catch((err) => res.status(err.statusCode).send(err));
 		})
-		.catch(async err => {
+		.catch(async (err) => {
 			//If error, force reset of access token
 			let credentials = JSON.parse(req.query.credentials);
 			credentials.expiresAt = 0;
@@ -130,19 +130,19 @@ app.get("/search-playlists", async (req, res) => {
 	const token = await accessToken(req.query.credentials);
 	const api = spotifyApiWithToken(token);
 	api.searchPlaylists(req.query.q)
-		.then(response => {
+		.then((response) => {
 			res.send(response);
 		})
-		.catch(err => res.status(err.statusCode).send(err));
+		.catch((err) => res.status(err.statusCode).send(err));
 });
 app.put("/playlist-details", async (req, res) => {
 	const token = await accessToken(req.body.data.credentials);
 	const api = spotifyApiWithToken(token);
 	getPlaylistTracks([req.body.data.playlist], api)
-		.then(response => {
+		.then((response) => {
 			res.send(getIDsFromTracks(response));
 		})
-		.catch(err => res.status(err.statusCode).send(err));
+		.catch((err) => res.status(err.statusCode).send(err));
 });
 
 //Get playlists user created with Run BPM
@@ -150,10 +150,10 @@ app.get("/playlists", function(req, res) {
 	const db = new UserDB(DatabasePath);
 	let userID = req.query.id;
 	db.getCreatedPlaylists(userID)
-		.then(response => {
+		.then((response) => {
 			res.send(response);
 		})
-		.catch(err => res.send(err));
+		.catch((err) => res.send(err));
 });
 
 //Add a new playlist to Run BPM's database
@@ -162,10 +162,10 @@ app.post("/playlists", async (req, res) => {
 	const api = spotifyApiWithToken(token);
 	let request = req.body.data;
 	createPlaylist(request.userID, request.name, request.trackIDs, request.metadata, api, request.image)
-		.then(response => {
+		.then((response) => {
 			res.status(201).send(response);
 		})
-		.catch(err => res.send("Something went wrong. Error: " + err));
+		.catch((err) => res.send("Something went wrong. Error: " + err));
 });
 
 app.put("/playlists", async (req, res) => {
@@ -177,7 +177,7 @@ app.put("/playlists", async (req, res) => {
 		.then(() => {
 			res.status(201).send();
 		})
-		.catch(err => res.send(err));
+		.catch((err) => res.send(err));
 });
 
 //Delete a playlist
@@ -190,12 +190,12 @@ app.delete("/playlists", async (req, res) => {
 		.then(() => {
 			//Delete the playlist from the db
 			db.deletePlaylist(req.query.id)
-				.then(response => {
+				.then((response) => {
 					res.status(204).send();
 				})
-				.catch(err => res.send(err));
+				.catch((err) => res.send(err));
 		})
-		.catch(err => res.send(err));
+		.catch((err) => res.send(err));
 });
 
 //Get audio features for many tracks
@@ -203,7 +203,7 @@ app.post("/analyze-tracks", async (req, res) => {
 	const token = await accessToken(req.body.data.credentials);
 	const api = spotifyApiWithToken(token);
 	getPlaylistTracks(req.body.data.playlists, api)
-		.then(response => {
+		.then((response) => {
 			const playlistDetails = response;
 			const trackDetails = [];
 			const trackIDs = _.chunk(getIDsFromTracks(response), 100);
@@ -211,23 +211,23 @@ app.post("/analyze-tracks", async (req, res) => {
 				trackDetails.push(
 					new Promise((resolve, reject) => {
 						api.getAudioFeaturesForTracks(trackIDs[i])
-							.then(data => {
+							.then((data) => {
 								resolve(data.body.audio_features);
 							})
-							.catch(err => reject(err));
+							.catch((err) => reject(err));
 					})
 				);
 			}
 			Promise.all(trackDetails)
-				.then(response => {
+				.then((response) => {
 					res.send({
 						playlistDetails: playlistDetails,
-						audioFeatures: _.flatten(response)
+						audioFeatures: _.flatten(response),
 					}); //used to have 'deets ?' taking out, see if it breaks anything.
 				})
-				.catch(err => res.send(err));
+				.catch((err) => res.send(err));
 		})
-		.catch(err => res.send(err));
+		.catch((err) => res.send(err));
 });
 
 //Get audio features for indevidual tracks
@@ -235,10 +235,10 @@ app.get("/analyze-tracks", async (req, res) => {
 	const token = await accessToken(req.query.credentials);
 	const api = spotifyApiWithToken(token);
 	api.getAudioFeaturesForTrack(req.query.id)
-		.then(response => {
+		.then((response) => {
 			res.send(response.body);
 		})
-		.catch(err => res.send(err));
+		.catch((err) => res.send(err));
 });
 
 //Control playback
@@ -250,23 +250,23 @@ app.put("/player", async (req, res) => {
 	switch (action) {
 		case "play":
 			api.play(options)
-				.then(response => res.status(response.statusCode).send())
-				.catch(error => res.status(error.statusCode).send(error));
+				.then((response) => res.status(response.statusCode).send())
+				.catch((error) => res.status(error.statusCode).send(error));
 			break;
 		case "pause":
 			api.pause(options)
-				.then(response => res.status(response.statusCode).send())
-				.catch(error => res.status(error.statusCode).send(error));
+				.then((response) => res.status(response.statusCode).send())
+				.catch((error) => res.status(error.statusCode).send(error));
 			break;
 		case "next":
 			api.skipToNext()
-				.then(response => res.status(response.statusCode).send())
-				.catch(error => res.status(error.statusCode).send(error));
+				.then((response) => res.status(response.statusCode).send())
+				.catch((error) => res.status(error.statusCode).send(error));
 			break;
 		case "previous":
 			api.skipToPrevious()
-				.then(response => res.status(response.statusCode).send())
-				.catch(error => res.status(error.statusCode).send(error));
+				.then((response) => res.status(response.statusCode).send())
+				.catch((error) => res.status(error.statusCode).send(error));
 			break;
 	}
 });
@@ -277,10 +277,10 @@ app.get("/player", async (req, res) => {
 	const api = spotifyApiWithToken(token);
 	const query = req.query.q;
 	api.getMyCurrentPlayingTrack()
-		.then(response => {
+		.then((response) => {
 			res.send(response.body);
 		})
-		.catch(err => {
+		.catch((err) => {
 			res.send(err);
 		});
 });
@@ -298,10 +298,10 @@ function addToPlaylist(playlistID, trackIDs, api) {
 			addedTracks.push(
 				new Promise((resolve, reject) => {
 					api.addTracksToPlaylist(playlistID, chunkedTrackIDs[i])
-						.then(data => {
+						.then((data) => {
 							resolve();
 						})
-						.catch(err => reject(err));
+						.catch((err) => reject(err));
 				})
 			);
 		}
@@ -309,14 +309,14 @@ function addToPlaylist(playlistID, trackIDs, api) {
 			.then(() => {
 				resolve();
 			})
-			.catch(err => reject(err.message));
+			.catch((err) => reject(err.message));
 	});
 }
 
 function createPlaylist(userID, playlistName, trackIDs, metadata, api, image) {
 	return new Promise((resolve, reject) => {
 		//Create the playlist
-		api.createPlaylist(userID, playlistName).then(response => {
+		api.createPlaylist(userID, playlistName).then((response) => {
 			//Get the new playlists' ID
 			const playlistID = response.body.id;
 
@@ -334,24 +334,24 @@ function createPlaylist(userID, playlistName, trackIDs, metadata, api, image) {
 				addedTracks.push(
 					new Promise((resolve, reject) => {
 						api.addTracksToPlaylist(playlistID, chunkedTrackIDs[i])
-							.then(data => {
+							.then((data) => {
 								resolve();
 							})
-							.catch(err => reject(err));
+							.catch((err) => reject(err));
 					})
 				);
 			}
 			Promise.all(addedTracks)
-				.then(response => {
+				.then((response) => {
 					//Save the playlist to apps database
 					const db = new UserDB(DatabasePath);
 					db.savePlaylist(playlistID, userID, trackIDs, metadata)
-						.then(response => {
+						.then((response) => {
 							resolve();
 						})
-						.catch(err => reject(err.message));
+						.catch((err) => reject(err.message));
 				})
-				.catch(err => reject(err.message));
+				.catch((err) => reject(err.message));
 
 			// 	api.addTracksToPlaylist(playlistID, getURIsFromIDs(trackIDs))
 			// 		.then(response => {
@@ -388,29 +388,29 @@ function getPlaylistTracks(playlists, api) {
 									.then(function(data) {
 										resolve(data.body.items);
 									})
-									.catch(err => reject(err));
+									.catch((err) => reject(err));
 							})
 						);
 					}
 					Promise.all(arr)
-						.then(data => {
+						.then((data) => {
 							resolve(_.flatten(data));
 						})
-						.catch(err => reject(err));
+						.catch((err) => reject(err));
 				})
 			);
 		}
 		Promise.all(playlistDetails)
-			.then(data => {
+			.then((data) => {
 				resolve(_.flatten(data));
 			})
-			.catch(err => reject(err));
+			.catch((err) => reject(err));
 	});
 }
 
 function getIDsFromTracks(details) {
 	const trackIDs = [];
-	details.forEach(track => {
+	details.forEach((track) => {
 		trackIDs.push(track.track.id);
 	});
 	return trackIDs;
@@ -418,7 +418,7 @@ function getIDsFromTracks(details) {
 
 function getURIsFromIDs(IDs) {
 	const trackURIs = [];
-	IDs.forEach(ID => {
+	IDs.forEach((ID) => {
 		trackURIs.push("spotify:track:" + ID);
 	});
 	return trackURIs;
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<v-app>
 		<router-view />
+		<v-footer absolute class="body-2 py-4"
+			>©2020 Run BPM<v-spacer /> Created by
+			<a href="https://rileyhemphill.dev" target="_blank" class="ml-1">Riley Hemphill</a>
+		</v-footer>
 	</v-app>
 </template>
 
@@ -9,6 +13,6 @@ export default {
 	name: "App",
 	data: () => ({
 		//
-	})
+	}),
 };
 </script>
